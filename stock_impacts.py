8a209c58cf0730a590450c08051676d7c6c0571c
Jordan Garcie <jordan.garcie@gmail.com>
2019-11-05 02:14:13 -0500

    added 1000 tweets, outputs every tweet instead of at the end

--- stock_impacts.py
+++ stock_impacts.py
@@ -7,16 +7,15 @@ from si_functions import *
 # default variables
 impact_time = 1 # in hours
 trade_impact = 1 # in percentage 
-stock_name = 'AMZN'
+stock_name = 'BABA'
 file_loc = '/Volumes/Seagate Expansion Drive/Stocks Data'
 file_prefix = '/EQY_US_ALL_TRADE_'
 results_file = './results.txt'
 
 # global variables
 tweet_times = []
-trades = []
-trade_changes = []
 tweet_counter = 1
+res = open(results_file, "w+")
 
 # read tweet datetimes from txt
 f = open("tweet_times.txt", "r")
@@ -51,34 +50,20 @@ for tt in tweet_times:
 
     # calculate stock price difference and save for later
     if 'time' in trade_before and 'time' in trade_after:
-        trades.append({"tb_time": trade_before["time"], "tb_price": trade_before["price"], \
-        "ta_time": trade_after["time"], "ta_price": trade_after["price"]})
         price_diff = (float(trade_after["price"]) - float(trade_before["price"])) / float(trade_before["price"])
-        trade_changes.append(price_diff * 100)
+        impacted = False
+        if abs(price_diff * 100) > trade_impact:
+            impacted = True
+
+        res.write("TWEET TIME: " + tt_adjusted.strftime('%m/%d/%Y %I:%M:%S %p') + " IMPACTED: " + str(impacted) \
+        + " CHANGE: " + str(round(price_diff * 100, 3)) + "% (FROM: " + trade_before["time"].strftime('%m/%d/%Y %H:%M:%S') \
+        + " - $" + trade_before["price"] + "; TO: " + trade_after["time"].strftime('%m/%d/%Y %H:%M:%S') \
+        + " - $" + trade_after["price"] + ")" + '\n')
     else:
-        trades.append(False)
-        trade_changes.append(False)
+        res.write("TWEET TIME: " + tt_adjusted.strftime('%m/%d/%Y %I:%M:%S %p') + " IMPACTED: False\n")
     
     print(str(tweet_counter) + "/" + str(tweet_total))
     tweet_counter += 1
-
-# check for tweet impacts per timestamp and write to results file
-res = open(results_file, "w+")
-for i, tt in enumerate(tweet_times):
-    tt_adjusted = tt - timedelta(hours=5) # GMT to EST adjustment
-    impacted = False
-    if trade_changes[i] is False:
-        impacted = 'N/A'
-    elif abs(trade_changes[i]) > trade_impact:
-        impacted = True
-    
-    if trades[i] is not False:
-        res.write("TWEET TIME: " + tt_adjusted.strftime('%m/%d/%Y %I:%M:%S %p') + " IMPACTED: " + str(impacted) \
-        + " CHANGE: " + str(round(trade_changes[i], 3)) + "% (FROM: " + trades[i]["tb_time"].strftime('%m/%d/%Y %H:%M:%S') \
-        + " - $" + trades[i]["tb_price"] + "; TO: " + trades[i]["ta_time"].strftime('%m/%d/%Y %H:%M:%S') \
-        + " - $" + trades[i]["ta_price"] + ")" + '\n')
-    else:
-        res.write("TWEET TIME: " + tt_adjusted.strftime('%m/%d/%Y %I:%M:%S %p') + " IMPACTED: " + str(impacted) + '\n')
 
 res.close()
 print('Finished!')
