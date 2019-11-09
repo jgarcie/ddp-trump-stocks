d96b9c897733776b2425aceb77a8043fa1a89ea2
Jordan Garcie <jordan.garcie@gmail.com>
2019-11-08 23:02:45 -0500

    fixing our naive bayes issues

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,8 +1,11 @@
 import csv
+import math
 
 impact_dict = "./dictionaries/TrueDict.csv"
 nonimpact_dict = "./dictionaries/FalseDict.csv"
-tweets = "./tweets/TestTweet2.csv"
+impact_word_count = 1995
+nonimpact_word_count = 2215
+tweets = "./tweets/final100tweets.csv"
 res = open("./nb_results.txt", "w+")
 
 with open(tweets) as tweets_csv:
@@ -18,18 +21,22 @@ with open(tweets) as tweets_csv:
                 with open(impact_dict) as impact_csv:
                     impact_reader = csv.reader(impact_csv, delimiter=",")
 
+                    impact_value += float(math.log(1/impact_word_count))
+
                     for imp_line in impact_reader:
                         if imp_line[0] == word:
-                            impact_value += float(imp_line[1])
+                            impact_value += float(imp_line[1]) - float(math.log(1/impact_word_count))
                             break
                     #end of for
                 
                 with open(nonimpact_dict) as nonimpact_csv:
                     nonimpact_reader = csv.reader(nonimpact_csv, delimiter=",")
 
+                    nonimpact_value += float(math.log(1/nonimpact_word_count))
+
                     for nonimp_line in nonimpact_reader:
                         if nonimp_line[0] == word:
-                            nonimpact_value += float(nonimp_line[1])
+                            nonimpact_value += float(nonimp_line[1]) - float(math.log(1/nonimpact_word_count))
                             break
                     #end of for
         #end of for
